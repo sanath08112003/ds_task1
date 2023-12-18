01a7da1fa5f45640bffd7bb6e40faf97ac6b1ec1
A.Sanath Kumar Sagar <153945752+sanath08112003@users.noreply.github.com>
2023-12-18 14:35:01 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,38 @@
+print("hello world")
+#this is the print statement
+
+#is the single line commment
+
+
+#variable means it is the container to store the value
+number=5
+#here "number" is the "variable" and we are assigning "5" to the "number"
+print(number)
+
+
+#data types
+section="a" #char
+print(section)
+studentname="sanath"#string
+print(studentname)
+studentage=20#integer
+print(studentage)
+grade=8.5#float
+print(grade)
+
+#casting 
+#changing one data type in other data type
+
+#here we are using "float datatype" in to "integer datatype"
+print(int(grade))
+#and now we are using"integer datatype" in to "float datatype"
+print(float(studentage))
+
+#string [slicing of string]
+#it gives the inder value 
+name=studentname[:6]
+print(name)
+
+#string[length of string]
+#it prints the length of string 
+print(len(name))
